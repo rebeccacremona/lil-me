804d5e791645af1e0427f57a8961ffceb5891b3b
rcremona <rebecca_cremona@harvard.edu>
2016-10-23 19:36:29 -0400

    Add hobbies and identities.

--- assets/static/bio.js
+++ assets/static/bio.js
@@ -1,3 +1,6 @@
+//
+// HELPERS
+//
 
 function shuffle(array) {
   //
@@ -25,7 +28,22 @@ function getRandomElement(list) {
     return list[Math.floor(Math.random() * list.length)];
 }
 
-function builtSomething(nickname) {
+function listToPhrase(list) {
+    if (list.length < 2) {
+       var link = '';
+    } else if (list.length == 2){
+       var link = ' and ';
+    } else {
+        var link = ', and '
+    }
+    return [list.slice(0, -1).join(', '), list.slice(-1)[0]].join(link)
+}
+
+//
+// BIO GENERATORS
+//
+
+function builtSomething(person) {
     var language = [
         'Pearl',
         'Python',
@@ -59,25 +77,127 @@ function builtSomething(nickname) {
     ];
 
     var shuffled = shuffle(components);
+    var list = [
+        getRandomElement(language) + " script",
+        shuffled[0],
+        shuffled[1]
+    ]
 
-    return document.createTextNode(nickname +
-                                   " once built a working " +
-                                   getRandomElement(built) +
-                                   " using only a " +
-                                   getRandomElement(language) +
-                                   " script, " + shuffled[0] +
-                                   ", and " + shuffled[1] + ".");
+    return person + " once built a working " +
+          getRandomElement(built) + " using only a " +
+          listToPhrase(list) + "."
+    ;
 
 }
 
+function aFewPhrases(type, count, person) {
+    var likes = [
+        'loves',
+        'enjoys',
+        'is into',
+        'digs'
+    ];
+
+    var hobbies = [
+        'orienteering',
+        'splunking',
+        'making lutefisk',
+        'renovating motorhomes',
+        'eating cheese',
+        'writing bogus git tutorials (mwahaha)',
+        'microwaving random stuff',
+        'making marshmallow sculptures',
+        'ironing ties',
+        'hunting down broad-leaved weeds',
+        'jaywalking in the rain',
+        'finding poetry in the wild',
+        'indiscriminately firing supersoakers',
+        'cleaning grout',
+        'climbing up the slide',
+        'folding clean towels',
+        'skeet skating',
+        'fishing for carp'
+    ];
+
+    var identities = [
+        'a peanut enthusiast',
+        'an amateur hanglider',
+        'an ex-con',
+        'a mathematician',
+        'former class president',
+        'an Olympic curler',
+        'a volunteer zookeeper',
+        'an award-winning zither player',
+        'a wainwright',
+        'a sock enthusiast',
+        'an infamous sneak',
+        'a haberdasher',
+        'a former seltzer artist',
+        'an award-winning monkey trainer',
+        'a big fan of sheep',
+        'a rockstar tax analyst',
+        'an arachnophobe',
+        'a determined solitaire player',
+        'a reckless driver',
+        'a proponent of tart citrus',
+        'a big fan of ardvarks',
+        'a refactoring machine',
+        'a universal maven',
+        'a knot-loosening specialist',
+    ];
+
+    if(type == 'identities'){
+       var shuffled = shuffle(identities);
+       var verb = "is";
+   } else {
+       var shuffled = shuffle(hobbies);
+       var verb = getRandomElement(likes);
+   }
+
+    var list = shuffled.slice(0,count)
+
+    return person + " " + verb + " " + listToPhrase(list) + ".";
+
+}
+
+//
+// Assembled Bios
+//
+
+function builder(bio) {
+    return document.createTextNode(
+        aFewPhrases("identities", 2, bio.dataset.name) +
+        " " +
+        builtSomething(bio.dataset.pronoun)
+    )
+}
+
+function hobbies(bio) {
+    return document.createTextNode(
+        aFewPhrases("identities", 3, bio.dataset.name) +
+        " " +
+        aFewPhrases("hobbies", 2, bio.dataset.pronoun)
+    )
+}
+
+//
+// DOM Manipulation
+//
+
 window.onload = function () {
+    var bioOptions = [
+        builder,
+        hobbies
+    ]
+
     var bios = document.getElementsByClassName("bio");
     for (var index in bios) {
         if (!bios.hasOwnProperty(index)) {
             continue;
         }
         var bio = bios[index];
-        bio.appendChild(builtSomething(bio.dataset.name));
+        var bioType = getRandomElement(bioOptions)
+        bio.appendChild(bioType(bio));
     }
 }
 
